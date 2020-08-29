de6e0103438df7bf60374f377a2cd809774ede0a
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-08-29 07:03:06 +0100

    Make vote tot jinja

--- votes/urls.py
+++ votes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import ListCreateVote
 urlpatterns = [
     path('', ListCreateVote.as_view()),
+    path('view/', TemplateView.as_view(template_name='vote.html')),
 ]
