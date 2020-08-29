4374e85d42902455cf0485bf5fb898fcf9584661
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-08-29 16:09:48 +0100

    Fix model

--- votes/models.py
+++ votes/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 class Vote(models.Model):
-    facebook_id = models.CharField(max_length=255, unique=True,
-                                   error_messages="A vote has already been cast with this facebook account.")
-    facebook_token = models.CharField(max_length=255)
+    facebook_id = models.CharField('Facebook account', max_length=255, unique=True)
+    facebook_token = models.CharField(max_length=255, unique=True)
     full_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     design_id = models.PositiveSmallIntegerField()
