389fe02b7eac4fab4e5cde70e0cecfc8e7352b4c
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-08-29 22:47:05 +0100

    Add guides page intermediary

--- votes/urls.py
+++ votes/urls.py
@@ -3,5 +3,5 @@ from django.views.generic import TemplateView
 
 from .views import ListCreateVote, VotingView
 urlpatterns = [
-    path('', VotingView.as_view()),
+    path('', VotingView.as_view(), name='polls'),
 ]
