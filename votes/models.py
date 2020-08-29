578fa032234fccc6ef7d224224c730ee06136725
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2020-08-29 16:03:33 +0100

    Better error message

--- votes/models.py
+++ votes/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class Vote(models.Model):
-    facebook_id = models.CharField(max_length=255, unique=True)
-    facebook_token = models.CharField(max_length=255, unique=True)
+    facebook_id = models.CharField(max_length=255, unique=True,
+                                   error_messages="A vote has already been cast with this facebook account.")
+    facebook_token = models.CharField(max_length=255)
     full_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     design_id = models.PositiveSmallIntegerField()
