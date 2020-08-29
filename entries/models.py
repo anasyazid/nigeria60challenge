b69b0bcf5b821f9c61ffceac59c2dcc222d129da
techadam <abdulflezy13@gmail.com>
2020-08-29 19:54:34 +0100

    Team memeber feature fix

--- entries/models.py
+++ entries/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Person(models.Model):
+    team_members = models.CharField(max_length=255, blank=True, null=True)
     fullname = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=255)
     email = models.CharField(max_length=255, unique=True)
--- entries/static/assets/js/entries.js
+++ entries/static/assets/js/entries.js
@@ -1,11 +1,37 @@
-/* =========== Handle Challenge Choice Toggle ================== */
+/* =========== Variable declarations ================== */
+const isTeams = document.querySelectorAll(".is-team");
+const teamWrapper = document.querySelector(".team-wrapper");
+const teamWrapperInput = document.querySelectorAll(".team-wrapper input");
 const choiceElem = document.querySelector('.challenge-choice');
 const poemContainer = document.querySelectorAll('.poetry');
 const sloganContainer = document.querySelectorAll('.slogan');
 const photoContainer = document.querySelectorAll('.photo');
+let tabContent = document.querySelector("#tab-content");
 
+/* =========== Check if there are team members for entries ================== */
+isTeams.forEach(team => {
+    team.addEventListener('change', () => {
+        tabContent.style.height = "auto"
+        if(team.checked && team.value !== 'yes') {
+            teamWrapper.classList.add('hidden');
+            teamWrapperInput.forEach(elem => {
+                elem.classList.remove('input-0');
+            })
+        }else{
+            teamWrapper.classList.remove('hidden');
+            teamWrapperInput.forEach(elem => {
+                elem.classList.add('input-0');
+            })
+        }
+    });
+});
+
+/* =========== Handle Challenge Choice Toggle ================== */
 choiceElem.addEventListener('change', (e) => {
     const choice = e.target.value.toLowerCase();
+    //Update wizard height
+    tabContent.style.height = "auto"
+
     if(choice) choiceForm(choice);
 });
 
@@ -61,6 +87,9 @@ const memberCotantainer = document.querySelector('#member');
 addMemberBtn.addEventListener('click', (e) => {
     const members = document.querySelectorAll('.members');
     const nodeIndex = members.length;
+
+    //Update wizard height
+    tabContent.style.height = "auto"
     
     memberCotantainer.innerHTML = memberCotantainer.innerHTML + `<div class="mt-4 col-sm-6 member-remove">
         <input placeholder="Member Name" type="text" name="member[]" required="" class="members input-0"/>
