30b3b8920b2ef9be7227716c7c832b7a509907fa
techadam <abdulflezy13@gmail.com>
2020-08-30 11:34:21 +0100

    Fix date of birth for entries and entries models update

--- entries/urls.py
+++ entries/urls.py
@@ -5,6 +5,7 @@ from .views import ListCreateEntries, EntryExportView, EntrySubmission
 urlpatterns = [
     path('', EntrySubmission.as_view(), name='entry_submission'),
     path('guide/', TemplateView.as_view(template_name='guide.html'), name='entries_guide'),
+    path('entry-submitted/', TemplateView.as_view(template_name='entrysuccess.html'), name='entries_success'),
     # path('', ListCreateEntries.as_view()),
     path('export/', EntryExportView.as_view()),
     path('<int:id>', ListCreateEntries.as_view()),
